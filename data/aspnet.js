815347d671f7559d4f3575eb2f745c3e3bfa8db1
ThomE-immalle <thom.ernst@immalle.be>
2017-04-20 18:16:53 +0200

    Added all questions to data/aspnet.js

--- data/aspnet.js
+++ data/aspnet.js
@@ -6,112 +6,112 @@
     },
     "questions": [{
         "Id": 1010,
-        "Name": "",
+        "Name": "On parle quelle langue en Flandre?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Exception", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Code-behind", "IsAnswer": true },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Code-front", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "français", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "néerlandais", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "anglais", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "allemand", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1011,
-        "Name": "",
+        "Name": "Qui gagna la bataille des Eperons d'Or?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Theme", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Code-front", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Cross Page Posting", "IsAnswer": true },
-            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "le roi Philippe IV", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Godefroid de Brabant", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "les Hennuyers de Jean Sans-Merci ", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "le roi d'Angleterre", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1012,
-        "Name": "",
+        "Name": "Où eut lieu la bataille des Eperons d'Or ?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "System.Web.UI.Page", "IsAnswer": true },
-            { "Id": 1057, "QuestionId": 1010, "Name": "System.Web.UI.Form", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "System.Web.GUI.Page", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "System.Web.Form", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "dans la plaine de Groeninghe", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "dans la banlieue de Bruges", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "près de Tournai", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "dans la plaine de l'Yser", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1013,
-        "Name": "",
+        "Name": "Qu'est-ce qu’un \"Goedendag\"?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Model View Controller", "IsAnswer": true },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Maximum Virtual Control", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Microsoft Visual Core", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "une hache", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "une catapulte", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "un javelot", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "un bâton muni de pointes en fer", "IsAnswer": true }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1014,
-        "Name": "",
+        "Name": "Quelle expression les adversaires devaient-ils prononcer en néerlandais pendant la Bataille de Bouvines?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Session", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "TempData", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "ViewData", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "ViewBag", "IsAnswer": true }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "gereedschapskist", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "schild en vriend", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "paard en kar", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "groenten en fruit", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1015,
-        "Name": "",
+        "Name": "Le port de la ville qui s’appelle … était le plus important du monde au seizième siècle. ",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Init, PreRender, Load", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Load, PreRender, Init", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Init, Load, PreRender", "IsAnswer": true },
-            { "Id": 1058, "QuestionId": 1010, "Name": "None of the above", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "Anvers", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Bruges", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Bruxelles", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Gand", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1016,
-        "Name": "Which of these data source controls do not implement caching?", 
+        "Name": "Quel instrument a été inventé en Flandre?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "ObjectDataSource", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "LinqDataSource", "IsAnswer": true },
-            { "Id": 1057, "QuestionId": 1010, "Name": "SqlDataSource", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "XmlDataSource", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "le cornet", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "la flûte", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "le hautbois", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "le saxophone ", "IsAnswer": true }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1017,
-        "Name": "",
+        "Name": "Quel est l’animal représenté sur le drapeau flamand?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "div", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "span", "IsAnswer": true },
-            { "Id": 1057, "QuestionId": 1010, "Name": "body", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "label", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "un coq", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "un lion", "IsAnswer": true },
+            { "Id": 1057, "QuestionId": 1010, "Name": "une hirondelle ", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "un cerf", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1018,
-        "Name": "",
+        "Name": "Quel sport présumer d’être anglais et invité par les flamands?",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Session.Terminate()", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Session.TimeOut()", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Session.Abondon()", "IsAnswer": true },
-            { "Id": 1058, "QuestionId": 1010, "Name": "Session.Kill()", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "le football", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "le golf", "IsAnswer": false },
+            { "Id": 1056, "QuestionId": 1010, "Name": "le cricket", "IsAnswer": true },
+            { "Id": 1058, "QuestionId": 1010, "Name": "le rugby", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     },
     {
         "Id": 1019,
-        "Name": "",
+        "Name": "Qui est le peintre flamand le plus connu? ",
         "QuestionTypeId": 1,
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "HttpContext", "IsAnswer": true },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Session", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Cookies", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "SqlServer", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "Pieter Breughel", "IsAnswer": true },
+            { "Id": 1056, "QuestionId": 1010, "Name": "Vincent van Gogh", "IsAnswer": false },
+            { "Id": 1057, "QuestionId": 1010, "Name": "Pablo Semper Ballester", "IsAnswer": false },
+            { "Id": 1058, "QuestionId": 1010, "Name": "Pablo Picasso", "IsAnswer": false }],
         "QuestionType": { "Id": 1, "Name": "Multiple Choice", "IsActive": true }
     }]
 }
